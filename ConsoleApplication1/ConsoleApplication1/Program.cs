00ee1c7a3f5168a5e3729ae2292a3c8d6f350c70
akira <ringo1625@gmail.com>
2016-10-07 20:38:40 +0900

    最初のコミット

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿// テキストファイルを開いて、表示するプログラム
+
+using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a;
+            Console.WriteLine("文字を入力してください。");
+            a = Console.ReadLine();
+            
+            // ファイルにテキストを書き出し。
+            using (StreamWriter w = new StreamWriter(@"D:\test.txt"))
+            {
+                w.WriteLine(a);
+            }
+
+            Console.WriteLine(@"D:\test.txtに保存しました。");
+            Console.WriteLine("");
+            Console.WriteLine("保存した内容を表示します。");
+
+
+            // ファイルからテキストを読み出し。
+            using (StreamReader r = new StreamReader(@"D:\test.txt"))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null) // 1行ずつ読み出し。
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine("何か文字を入力すると終了します。");
+            Console.ReadKey();
+        }
+    }
+}
